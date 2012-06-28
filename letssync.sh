6cd73d2caa17823be276614edf96ebd98e6e13a3
Roman Charugin <roman.charugin@gmail.com>
2012-06-29 01:30:33 +0400

    Added usage message; Added login and password as parameters; Have added checking folder_to_sync parameter is NULL

--- letssync.sh
+++ letssync.sh
@@ -1,19 +1,77 @@
 #!/bin/sh
 # Setting up the sync with tds file-storage
 
-# Asking user for login&password
-echo "Enter your login: "
-read login
-echo "Enter your password: "
-read password
+# Usage message
+usage() {
+	cat << EOF
+
+usage: $0 [options] folder_to_sync
+
+$0 setup crontab task for syncing with TDS-dropbox via rsync
+
+OPTIONS:
+  -u	Login (would be promted, if not given)
+  -p	Password (would be promted, if not given)
+  -h	Print this message
+
+EOF
+}
+
+# Parsing input parametrs
+login=
+password=
+
+while getopts "u:p:h" opt; do
+case $opt in
+	u)
+		login=$OPTARG
+		;;
+	p)
+		password=$OPTARG
+		;;
+	h)
+		usage
+		exit 0
+		;;
+	?)
+		usage
+		exit 1
+		;;
+esac
+done
+
+# Getting last parametr – path to folder to sync
+shift $((OPTIND-1))
+folder=$@
+
+# Checking if folder to sync is given
+if [ -z $folder ]
+then
+	echo "ERROR >>> folder_to_sync is undefined!"
+	usage
+	exit 1
+fi
+
+# Asking user for login&password if they not given
+if [ -z $login ]
+then
+	echo "Enter your login: "
+	read login
+fi
+
+if [ -z $password ]
+then
+	echo "Enter your password: "
+	read password
+fi
 
 # Saving password to file
 echo $password > $HOME/.rsyncpass
 # Setup sasafity permission to the file with password
 chmod 640 $HOME/.rsyncpass
 
-# Getting path to sync-folder
-folder=$1
+echo "$rsync --update --progress --times --exclude-from $HOME/.rsyncexclude --recursive --stats --password-file=$HOME/.rsyncpass $folder/ rsync://$login@tech.tdigitals.ru/dropbox/"
+exit
 
 # Creating file with exclude rules
 echo ".*" > $HOME/.rsyncexclude
